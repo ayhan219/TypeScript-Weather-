2f0e3b08f20606aa08772d0f6d239a24af045eef
Ayhan <ayhan-games@hotmail.com>
2024-11-25 22:44:30 +0300

    sidebar added and updated

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { MdTornado } from "react-icons/md";
+import { TiWeatherCloudy } from "react-icons/ti";
+import { PiCityBold } from "react-icons/pi";
+import { FaMap } from "react-icons/fa";
+import { CiSettings } from "react-icons/ci";
+
+type Props = {}
+
+const SideBar = (props: Props) => {
+    const [activeMenu,setActiveMenu] = useState<string>("weather");
+
+
+
+    const changeActive = (e: React.MouseEvent<HTMLDivElement>, parameter: string)=>{
+        e.preventDefault();
+        setActiveMenu(parameter);
+    }
+  return (
+    <div className='w-32 h-full bg-[#202B3D] rounded-lg'>
+        <div className='text-5xl text-white flex justify-center pt-4'>
+            <MdTornado />
+        </div>
+        <div className='text-white flex flex-col gap-6 pt-10 '>
+            <div onClick={(e)=>changeActive(e,"weather")} className={`flex flex-col text-xl items-center cursor-pointer ${activeMenu==="weather" && "text-yellow-300"}`}>
+                <TiWeatherCloudy className='text-4xl' />
+                <a href="">Weather</a>
+            </div>
+            <div onClick={(e)=>changeActive(e,"cities")} className={`flex flex-col text-xl items-center cursor-pointer ${activeMenu==="cities" && "text-yellow-300"}`}>
+                <PiCityBold className='text-4xl' />
+                <a href="">Cities</a>
+            </div>
+            <div onClick={(e)=>changeActive(e,"map")} className={`flex flex-col text-xl items-center cursor-pointer ${activeMenu==="map" && "text-yellow-300"}`}>
+                <FaMap className='text-4xl' />
+                <a href="">Map</a>
+            </div>
+            <div onClick={(e)=>changeActive(e,"settings")} className={`flex flex-col text-xl items-center cursor-pointer ${activeMenu==="settings" && "text-yellow-300"}`}>
+                <CiSettings className='text-4xl' />
+                <a href="">Settings</a>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default SideBar
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import SideBar from '../components/SideBar'
 
 type Props = {}
 
 const Home = (props: Props) => {
   return (
-    <div className='w-full h-screen bg-blue-950'>
-
+    <div className='w-full h-screen bg-[#0B121E] p-8'>
+        <SideBar />
     </div>
   )
 }
