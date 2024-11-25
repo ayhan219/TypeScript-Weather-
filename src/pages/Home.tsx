33a7eb46e4f7211d2a7270b1046fafad6f10d186
Ayhan <ayhan-games@hotmail.com>
2024-11-25 22:12:04 +0300

    home page created

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <div className='w-full h-screen bg-blue-950'>
+
+    </div>
+  )
+}
+
+export default Home
