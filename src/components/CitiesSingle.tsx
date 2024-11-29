3e2ec30705d992594017126c4c9f479f686372f6
Ayhan <ayhan-games@hotmail.com>
2024-11-29 23:20:45 +0300

    citiessingle updated

--- src/components/CitiesSingle.tsx
+++ src/components/CitiesSingle.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 
-type Props = {}
 
-const CitiesSingle = (props: Props) => {
+
+const CitiesSingle = () => {
   return (
     <div>
-        <div className='w-96 h-24 bg-[#202B3D] flex items-center rounded-lg justify-between'>
+        <div className='w-96 h-24 bg-[#202B3D] flex items-center rounded-lg justify-between cursor-pointer hover:bg-[#333f53] ease-in-out duration-100 '>
             <div className='w-[40%] h-auto flex items-center justify-evenly'>
               <div>
                 <img className='w-16 h-16' src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png" alt="" />
