ef7640f9f9d69b75cf6ae4d2ea65a640a663bb0f
Ayhan <ayhan-games@hotmail.com>
2024-11-30 02:33:56 +0300

    app.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import SideBar from "./components/SideBar"
 import { WeatherProvider } from "./context/WeatherContext"
 import Cities from "./pages/Cities"
 import Map from "./pages/Map"
+import Settings from "./pages/Settings"
 function App() {
   
 
@@ -16,6 +17,7 @@ function App() {
     <Route path="/" element={<Home />} />
     <Route path="/cities" element={<Cities />} />
     <Route path="/map" element={<Map />} />
+    <Route path="/settings" element={<Settings />} />
     
    </Routes>
    </WeatherProvider>
