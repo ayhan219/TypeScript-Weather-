6005b92071c05de72951f5daa4dcd8f98c08ff4c
Ayhan <ayhan-games@hotmail.com>
2024-11-29 23:19:13 +0300

    cities updated

---
+++ src/components/CitiesSingle.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+type Props = {}
+
+const CitiesSingle = (props: Props) => {
+  return (
+    <div>
+        <div className='w-96 h-24 bg-[#202B3D] flex items-center rounded-lg justify-between'>
+            <div className='w-[40%] h-auto flex items-center justify-evenly'>
+              <div>
+                <img className='w-16 h-16' src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png" alt="" />
+              </div>
+              <div className='font-bold text-white'>
+                <h3>Madrid</h3>
+                <span className='text-[#656F81]'>10:23</span>
+              </div>
+            </div>
+            <div className='font-semibold text-2xl text-white pr-5'>
+              <h1>30°</h1>
+            </div>
+          </div>
+    </div>
+  )
+}
+
+export default CitiesSingle
--- src/pages/Cities.tsx
+++ src/pages/Cities.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import MapArea from '../components/MapArea'
+
+import CitiesSingle from '../components/CitiesSingle'
 
 
 type Props = {}
@@ -8,6 +8,28 @@ const Cities = (props: Props) => {
   return (
     
     <div className='w-full h-screen bg-[#0B121E]'>
+      <div className='w-full h-auto flex items-center text-3xl font-bold text-white justify-center pt-5'>
+        <h1>Most biggest cities</h1>
+      </div>
+      <div className='w-full h-[90%] flex'>
+        <div className='w-full h-full flex items-end pt-5 flex-col gap-4 pr-5 '>
+          <CitiesSingle />
+          <CitiesSingle />
+          <CitiesSingle />
+          <CitiesSingle />
+          <CitiesSingle />
+          <CitiesSingle />
+
+        </div>
+        <div className='w-full h-full flex items-start pt-5 flex-col gap-4 pl-5 '>
+        <CitiesSingle />
+          <CitiesSingle />
+          <CitiesSingle />
+          <CitiesSingle />
+          <CitiesSingle />
+          <CitiesSingle />
+        </div>
+      </div>
       
     </div>
   )
