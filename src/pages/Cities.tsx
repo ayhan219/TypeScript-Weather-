4dd74930efdbf944056ebc2e0310b9046faa4b86
Ayhan <ayhan-games@hotmail.com>
2024-11-30 02:01:13 +0300

    cities updated for responsive design

--- src/pages/Cities.tsx
+++ src/pages/Cities.tsx
@@ -7,11 +7,11 @@ type Props = {}
 const Cities = (props: Props) => {
   return (
     
-    <div className='w-full h-screen bg-[#0B121E]'>
+    <div className='w-full h-auto md:h-screen bg-[#0B121E]'>
       <div className='w-full h-auto flex items-center text-3xl font-bold text-white justify-center pt-5'>
         <h1>Weather conditions of major cities</h1>
       </div>
-      <div className='w-full h-[90%] flex'>
+      <div className='w-full h-[90%] flex flex-col md:flex-row'>
         <div className='w-full h-full flex items-end pt-5 flex-col gap-4 pr-5 '>
           <CitiesSingle />
           <CitiesSingle />
@@ -21,7 +21,7 @@ const Cities = (props: Props) => {
           <CitiesSingle />
 
         </div>
-        <div className='w-full h-full flex items-start pt-5 flex-col gap-4 pl-5 '>
+        <div className='w-full h-full flex items-end md:items-start pt-5 flex-col gap-4 pr-5 md:pr-0  '>
         <CitiesSingle />
           <CitiesSingle />
           <CitiesSingle />
