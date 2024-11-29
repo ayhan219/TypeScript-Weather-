270b12d8706d5aeb2ba3936e451f842976eade70
Ayhan <ayhan-games@hotmail.com>
2024-11-30 02:02:34 +0300

    citiessingle updated for responsive design

--- src/components/CitiesSingle.tsx
+++ src/components/CitiesSingle.tsx
@@ -4,7 +4,7 @@
 const CitiesSingle = () => {
   return (
     <div>
-        <div className='w-96 h-24 bg-[#202B3D] flex items-center rounded-lg justify-between cursor-pointer hover:bg-[#333f53] ease-in-out duration-100 '>
+        <div className='w-64 md:w-96 h-16 md:h-24 bg-[#202B3D] flex items-center rounded-lg justify-between cursor-pointer hover:bg-[#333f53] ease-in-out duration-100 '>
             <div className='w-[40%] h-auto flex items-center justify-evenly'>
               <div>
                 <img className='w-16 h-16' src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png" alt="" />
