7f6bbd5b597d2985363bb5df4b91f2b56b03b08a
Ayhan <ayhan-games@hotmail.com>
2024-11-30 02:05:05 +0300

    sidebar updated for responsive design

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -19,32 +19,32 @@ const SideBar = (props: Props) => {
         setActiveMenu(parameter);
     }
   return (
-    <div className=' w-24 md:w-32 h-[90%] bg-[#202B3D] mt-7 rounded-lg absolute'>
-        <div className='text-5xl text-white flex justify-center pt-4 cursor-pointer'>
+    <div className='w-16 md:w-32 h-[90%] bg-[#202B3D] mt-7 rounded-lg absolute'>
+        <div className='text-3xl md:text-5xl text-white flex justify-center pt-4 cursor-pointer'>
             <MdTornado />
         </div>
         <div className='text-white flex flex-col gap-6 pt-10 '>
             <Link to={"/"}>
-            <div onClick={(e)=>changeActive(e,"weather")} className={`flex flex-col text-xl items-center cursor-pointer ${activeMenu==="weather" && "text-yellow-300"}`}>
-                <TiWeatherCloudy className='text-4xl' />
+            <div onClick={(e)=>changeActive(e,"weather")} className={`flex flex-col text-base md:text-xl items-center cursor-pointer ${activeMenu==="weather" && "text-yellow-300"}`}>
+                <TiWeatherCloudy className='text-2xl md:text-4xl' />
                 <a>Weather</a>
             </div>
             </Link>
             <Link to={"/cities"}>
-            <div onClick={(e)=>changeActive(e,"cities")} className={`flex flex-col text-xl items-center cursor-pointer ${activeMenu==="cities" && "text-yellow-300"}`}>
-                <PiCityBold className='text-4xl' />
+            <div onClick={(e)=>changeActive(e,"cities")} className={`flex flex-col text-base md:text-xl items-center cursor-pointer ${activeMenu==="cities" && "text-yellow-300"}`}>
+                <PiCityBold className='text-2xl md:text-4xl' />
                 <a>Cities</a>
             </div>
             </Link>
             
             <Link to={"/map"}>
-            <div onClick={(e)=>changeActive(e,"map")} className={`flex flex-col text-xl items-center cursor-pointer ${activeMenu==="map" && "text-yellow-300"}`}>
-                <FaMap className='text-4xl' />
+            <div onClick={(e)=>changeActive(e,"map")} className={`flex flex-col text-base md:text-xl items-center cursor-pointer ${activeMenu==="map" && "text-yellow-300"}`}>
+                <FaMap className='text-2xl md:text-4xl' />
                 <a href="">Map</a>
             </div>
             </Link>
-            <div onClick={(e)=>changeActive(e,"settings")} className={`flex flex-col text-xl items-center cursor-pointer ${activeMenu==="settings" && "text-yellow-300"}`}>
-                <CiSettings className='text-4xl' />
+            <div onClick={(e)=>changeActive(e,"settings")} className={`flex flex-col text-base md:text-xl items-center cursor-pointer ${activeMenu==="settings" && "text-yellow-300"}`}>
+                <CiSettings className='text-2xl md:text-4xl' />
                 <a href="">Settings</a>
             </div>
 
